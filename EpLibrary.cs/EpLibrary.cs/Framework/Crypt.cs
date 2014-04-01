1cb9bdadb9676e1522a542f30c0e05cd96a766ff
Woong Gyu La <juhgiyo@gmail.com>
2014-04-01 22:31:17 +0900

    Crypt updated

--- EpLibrary.cs/EpLibrary.cs/Framework/Crypt.cs
+++ EpLibrary.cs/EpLibrary.cs/Framework/Crypt.cs
@@ -74,6 +74,19 @@ namespace EpLibrary.cs
         /// <returns>encrypted/decrypted data</returns>
         public static string GetCrypt(string cryptData, string cryptPwd, CryptType cryptType)
         {
+            return GetCrypt(cryptData, cryptPwd, null, cryptType);
+        }
+
+        /// <summary>
+        /// Encrypt/Decypt the given cryptData string with the given password
+        /// </summary>
+        /// <param name="cryptData">string data to encrypt</param>
+        /// <param name="cryptPwd">password string</param>
+        /// <param name="cryptType">crypt type</param>
+        /// <returns>encrypted/decrypted data</returns>
+        /// <remarks>if keySalt is null, then default keySalt is used</remarks>
+        public static string GetCrypt(string cryptData, string cryptPwd,byte[] keySalt, CryptType cryptType)
+        {
             try
             {
                 byte[] cryptBytes = null;
@@ -87,7 +100,7 @@ namespace EpLibrary.cs
                         break;
                 }
 
-                byte[] retData=GetCrypt(cryptBytes, cryptPwd, cryptType);
+                byte[] retData = GetCrypt(cryptBytes, cryptPwd,keySalt, cryptType);
                 switch (cryptType)
                 {
                     case CryptType.Encrypt:
@@ -99,7 +112,7 @@ namespace EpLibrary.cs
             catch
             {
             }
-            return null; 
+            return null;
         }
 
         /// <summary>
@@ -111,8 +124,22 @@ namespace EpLibrary.cs
         /// <returns>encrypted/decrypted data</returns>
         public static byte[] GetCrypt(byte[] cryptData, string cryptPwd, CryptType cryptType)
         {
-            byte[] keySalt = new byte[] { 0x54, 0x81, 0x45, 0x4A, 0x3B, 0x5E, 0x52, 0x15, 0x86, 0x5A, 0x40, 0x3B, 0xB4 };
+              return GetCrypt(cryptData, cryptPwd, null, cryptType);
+        }
 
+        /// <summary>
+        /// Encrypt/Decypt the given cryptData with the given password
+        /// </summary>
+        /// <param name="cryptData">data to crypt</param>
+        /// <param name="cryptPwd">password string</param>
+        /// <param name="keySalt">salt string</param>
+        /// <param name="cryptType">crypt type</param>
+        /// <returns>encrypted/decrypted data</returns>
+        /// <remarks>if keySalt is null, then default keySalt is used</remarks>
+        public static byte[] GetCrypt(byte[] cryptData, string cryptPwd, byte[] keySalt, CryptType cryptType)
+        {
+            if(keySalt==null)
+                   keySalt = new byte[] { 0x54, 0x81, 0x45, 0x4A, 0x3B, 0x5E, 0x52, 0x15, 0x86, 0x5A, 0x40, 0x3B, 0xB4 };
             PasswordDeriveBytes pwdBytes = new PasswordDeriveBytes(cryptPwd, keySalt);
 
             Rijndael crypAlg = Rijndael.Create();
@@ -133,6 +160,35 @@ namespace EpLibrary.cs
             {
             }
             return null;
+        }
+
+        /// <summary>
+        /// Create random salt with given length
+        /// </summary>
+        /// <param name="length">length of salt bytes</param>
+        /// <returns>randomly created salt</returns>
+        public static byte[] CreateRandomSalt(int length)
+        {
+            // Create a buffer 
+            byte[] randBytes;
+
+            if (length >= 1)
+            {
+                randBytes = new byte[length];
+            }
+            else
+            {
+                randBytes = new byte[1];
+            }
+
+            // Create a new RNGCryptoServiceProvider.
+            RNGCryptoServiceProvider rand = new RNGCryptoServiceProvider();
+
+            // Fill the buffer with random bytes.
+            rand.GetBytes(randBytes);
+
+            // return the bytes. 
+            return randBytes;
         }
     }
 }
