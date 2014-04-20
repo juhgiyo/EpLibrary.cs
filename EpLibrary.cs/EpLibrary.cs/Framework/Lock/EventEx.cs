cfc3b229a53a75cf5c31b736eccaca5a0c95f881
Woong Gyu La <juhgiyo@gmail.com>
2014-04-20 15:50:48 +0900

    Remove unnecesary import

--- EpLibrary.cs/EpLibrary.cs/Framework/Lock/EventEx.cs
+++ EpLibrary.cs/EpLibrary.cs/Framework/Lock/EventEx.cs
@@ -39,7 +39,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
